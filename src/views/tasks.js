f551a6926819209468024d16dba079b7a5c517e3
Alice <alicenjoroge707@gmail.com>
2020-03-09 14:20:06 +0300

    navbar, first commit

---
+++ src/views/tasks.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class Tasks extends Component{
+    render() {
+        return(
+            <div className="tasks">
+                <h1>Tasks</h1>
+            </div>
+        );
+    }
+}
+
+export default Tasks;
