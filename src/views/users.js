c9d0850f2d0fff7683f58d0a3bbe0a27f6517b90
Alice <alicenjoroge707@gmail.com>
2020-03-10 13:15:36 +0300

    delete a user, use sweetAlert for confirmation

--- src/views/users.js
+++ src/views/users.js
@@ -1,12 +1,20 @@
 import React, {Component} from "react";
 import axios from "axios";
+import swal from 'sweetalert';
+
 
 class Users extends Component {
     state = {
-        users: []
+        users: [],
+        success: null
     };
 
     componentDidMount() {
+        this.getUsers();
+
+    }
+
+    getUsers = (e) => {
         axios.get(`http://127.0.0.1:3031/users/`)
             .then(res => {
                 const users = res.data;
@@ -16,19 +24,54 @@ class Users extends Component {
             }).catch(e => {
             console.log(e);
         })
+    };
 
-    }
+    deleteUser = (id) => {
+        swal({
+            title: "Are you sure?",
+            text: "Once deleted, you will not be able to recover this User!",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        })
+            .then((willDelete) => {
+                if (willDelete) {
+                    axios.delete(`http://127.0.0.1:3031/users/${id}`)
+                        .then(res => {
+                            swal(res.data.message, {
+                                icon: "success",
+                            });
+                           this.getUsers();
+
+                        })
+                        .catch(e => {
+                            swal(e.response.data.message);
+                        });
+
+
+                } else {
+                    swal("User not deleted");
+                }
+            });
+
+
+
+
+    };
 
     render() {
         const userList = this.state.users.map(user => {
             return (
                 <tr key={user.id}>
-                    <th scope="row">1</th>
+                    <th scope="row">{user.id}</th>
                     <td>{user.name}</td>
                     <td>{user.email}</td>
                     <td>
                         <button type="button" className="btn btn-primary">Roles</button>
-                        <button type="button" className="btn btn-danger ml-2">Delete</button>
+                        <button type="button" onClick={() => {
+                            this.deleteUser(user.id)
+                        }} className="btn btn-danger ml-2">Delete
+                        </button>
                     </td>
                 </tr>
             )
